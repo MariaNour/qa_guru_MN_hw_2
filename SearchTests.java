22650d4fe279bff095b99c02c12e5c8eda381ef3
Maria_Nour <marinour@mail.ru>
2023-02-18 14:04:54 +0300

    URL was changed

--- SearchTests.java
+++ SearchTests.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class SearchTests {
     @Test
     void successfulSearchTest() {
-        open("https://www.google.com/");
+        open("https://ya.ru/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
